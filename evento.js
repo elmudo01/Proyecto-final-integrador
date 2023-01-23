ac28cb9edc730e7ecab439f1b8fccfde3e8799b4
elmudo01 <122963374+elmudo01@users.noreply.github.com>
2023-01-23 16:36:17 -0300

    Add files via upload

--- evento.js
+++ evento.js
@@ -1,35 +1,8 @@
-function myFunction() { 
-    document.getElementById("datos").style.backgroundColor =  "red";
-    document.getElementById("experiencia").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-    document.getElementById("estudios").style.backgroundColor = "rgba(59, 57, 57, 0.142)";
-    document.getElementById("habilidades").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-  }
-
-  function myFunction2() { 
-    document.getElementById("experiencia").style.backgroundColor =  "red";
-    document.getElementById("datos").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-    document.getElementById("estudios").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-    document.getElementById("habilidades").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-  }
-  function myFunction3() { 
-    document.getElementById("datos").style.backgroundColor = "rgba(59, 57, 57, 0.142)" ;
-    document.getElementById("experiencia").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-    document.getElementById("estudios").style.backgroundColor = "red";
-    document.getElementById("habilidades").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-  }
-  function myFunction4() { 
-    document.getElementById("experiencia").style.backgroundColor = "rgba(59, 57, 57, 0.142)";
-    document.getElementById("datos").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-    document.getElementById("estudios").style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-    document.getElementById("habilidades").style.backgroundColor =  "red";
-  }
-
- /* function prueba(p1,p2,p3,p4) { 
+function cabiarFondo(p1,p2,p3,p4) { 
     document.getElementById(p1).style.backgroundColor = "red";
     document.getElementById(p2).style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
     document.getElementById(p3).style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
     document.getElementById(p4).style.backgroundColor =  "rgba(59, 57, 57, 0.142)";
-  }*/
+  }
   
-  /*Mi idea era  usar la funcion prueba asi pasarle por parametros los elementos a cambiar el color de fondo
-  pero no encontre la forma de pasar los parametros en el HTML*/
+  
